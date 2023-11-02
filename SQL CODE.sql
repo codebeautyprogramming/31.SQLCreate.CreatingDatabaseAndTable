d272574b7c4dde3f67b8173fbc86cd5490aa51ce
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-02 22:19:47 +0100

    Startup code for this chapter

---
+++ SQL CODE.sql
@@ -0,0 +1,15 @@
+--###Chapter 31. SQL Create (creating databse and table)
+
+-- Remember to select command before executing it, otherwise management studio will try to execute 
+-- all commands at the same time which can cause unexpected issues.
+
+
+-- SQL command for creating database named CookBookDB.
+create database CookBookDB
+
+
+-- SQL command for selecting CookBookDB before writing queries
+use CookBookDB
+
+--###End of chapter
+
